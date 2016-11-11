7ee502a313372bac103e8fa6f222fc40b6587386
Matty Kit <put542.2@gmail.com>
2016-11-11 16:52:18 +0700

    0407 What are providers

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,8 @@
 angular
-  .module("ngClassifieds", []);
+  .module("ngClassifieds", ["ngMaterial"])
+  .config(function($mdThemingProvider) {
+
+    $mdThemingProvider.theme('default')
+      .primaryPalette('teal')
+      .accentPalette('orange');
+  });
